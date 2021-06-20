a214b9e3a97c0ee1130198dde10b75d477f13028
tranngoc769 <tranngoc769@gmail.com>
2021-06-20 19:50:57 +0700

    check exist, delete

--- build/gocrawl.sh
+++ build/gocrawl.sh
@@ -1,9 +1,6 @@
-#!/bin/sh
 echo "Copy environment file to env"
 yes | cp -rf build/gocrawler-env /root/go/env/gocrawler-env
 echo "Build go application service"
-go mod tidy
-go get .
 GOOS=linux GOARCH=amd64 go build -o gocrawler main.go
 echo "Restart service"
 systemctl restart gocrawler
